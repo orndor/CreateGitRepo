d0c26b8edd3840649dbfbc840ee0149ba05005ba
orndor <45725975+orndor@users.noreply.github.com>
2019-07-11 18:32:57 -0400

    Update
    Cleaned up something things...

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,16 +1,29 @@
-# Initialize GIT Repo for homework assignements
-#!/bin/bash
+# Initialize GIT Repo for homework assignements#
+# File name: Program.cs                        #
+# Project Name: repo-setup.sh                  #
+# Author: Orndoff, Robert K.                   #
+# Date created: 07/11/2019                     #
+# Date last modified: 07/11/2019               #
+#!/bin/bash                                    #
+################################################
+# Enter the full path where you will store your classwork C# projects. ** Make sure you include the trailing '/' on the directory. **
+fullPath=/c/root/folder/where/you/will/store/your/projects/
 echo "Enter Project Name:"
-read projname
-mkdir /c/Users/rornd/OneDrive/MSSA/Term-1/CSharp-Projects/$projname
-cd /c/Users/rornd/OneDrive/MSSA/Term-1/CSharp-Projects/$projname
-echo $projname >> README.md
-cp /c/Users/rornd/OneDrive/MSSA/Term-1/CSharp-Projects/.gitignore ./.gitignore
+read projName
+# Enter your GitHub Authentication Token.  Set this up at: https://github.com/settings/tokens
+authToken=PASTE-YOUR-TOKEN-OVER-THIS-ALL-CAP-TEXT
+# Enter GitHub username below:
+gitUsername=ENTER-USERNAME-HERE
+# Crunching starts below...
+mkdir $fullPath$projName
+cd $fullPath$projName
+echo $projName >> README.md
+cp $fullPath.gitignore .gitignore
 dotnet new console
 git init
 git add .
-git commit -m "first commit"
-curl -i -H 'Authorization: token INSERT-TOKEN-HERE' --data '{"name":"$projname"}' https://api.github.com/user/repos
-git remote add origin https://github.com/orndor/$projname.git
+git commit -m "First Commit"
+curl -i -H "Authorization: token $authToken" --data "{\"name\":\"$projName\"}" https://api.github.com/user/repos
+git remote add origin https://github.com/$gitUsername/$projName.git
 git push -u origin master
 echo "All Done!"
